82a6aecf41c29a3d1ddfc67832717354f8cb5687
Douglas Vanny Bento <douglas.vanny@gmail.com>
2014-06-09 01:07:45 -0300

    [#Bug Fix - Image rescale]
    Now images can be rescaled avoiding OutOfMemory Exceptions

--- src/info/beastarman/e621/middleware/ImageViewHandler.java
+++ src/info/beastarman/e621/middleware/ImageViewHandler.java
@@ -1,12 +1,17 @@
 package info.beastarman.e621.middleware;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -23,19 +28,46 @@ public class ImageViewHandler extends Handler
 		this.loader = loader;
 	}
 	
+	private Bitmap decodeFile(InputStream in, int width, int height)
+	{
+		byte[] data;
+		
+		try {
+			data = IOUtils.toByteArray(in);
+		} catch (IOException e) {
+			return null;
+		}
+		
+        //Decode image size
+        BitmapFactory.Options o = new BitmapFactory.Options();
+        o.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(new ByteArrayInputStream(data),null,o);
+
+        //Find the correct scale value. It should be the power of 2.
+        int scale=1;
+        while(o.outWidth/scale/2>=width && o.outHeight/scale/2>=height)
+        {
+        	scale*=2;
+        }
+        
+        //Decode with inSampleSize
+        BitmapFactory.Options o2 = new BitmapFactory.Options();
+        o2.inSampleSize=scale;
+        Bitmap bitmap_temp = BitmapFactory.decodeStream(new ByteArrayInputStream(data), null, o2);
+        
+        return Bitmap.createScaledBitmap(bitmap_temp,width,height,false);
+	}
+	
 	@Override
 	public void handleMessage(Message msg)
 	{
 		try
 		{
 			InputStream in = (InputStream)msg.obj;
-			Bitmap bitmap = BitmapFactory.decodeStream(in);
+			Bitmap bitmap = decodeFile(in, imgView.getLayoutParams().width, imgView.getLayoutParams().height);
 			in.close();
 			
-			int width = imgView.getLayoutParams().width;
-			int height = imgView.getLayoutParams().height;
-			
-			this.imgView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, width, height, false));
+			this.imgView.setImageBitmap(bitmap);
 		}
 		catch (Exception e)
 		{
