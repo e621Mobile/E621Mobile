226c2b5d372d471a12cc35a35c65e3dd9b56ab0a
Douglas Vanny Bento <douglas.vanny@gmail.com>
2014-07-01 01:44:33 -0300

    [#Bug Fix - Search loading time]
    Now search does not oerload UI thread

--- src/info/beastarman/e621/frontend/SearchActivity.java
+++ src/info/beastarman/e621/frontend/SearchActivity.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import info.beastarman.e621.R;
 import info.beastarman.e621.api.E621Image;
 import info.beastarman.e621.api.E621Search;
+import info.beastarman.e621.middleware.E621Middleware;
 import info.beastarman.e621.middleware.ImageLoadRunnable;
 import info.beastarman.e621.middleware.ImageViewHandler;
 import info.beastarman.e621.middleware.OnlineImageNavigator;
@@ -21,6 +22,9 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -204,7 +208,7 @@ public class SearchActivity extends BaseActivity
 
 	public void update_results()
 	{
-		LinearLayout layout = (LinearLayout) findViewById(R.id.content_wrapper);
+		final LinearLayout layout = (LinearLayout) findViewById(R.id.content_wrapper);
 		layout.removeAllViews();
 
 		if (e621Search == null) {
@@ -236,207 +240,283 @@ public class SearchActivity extends BaseActivity
 			return;
 		}
 
-		int layout_width = layout.getWidth();
+		final int layout_width = layout.getWidth();
 		
-		LazyRunScrollView scroll = (LazyRunScrollView)findViewById(R.id.resultsScrollView);
+		final LazyRunScrollView scroll = (LazyRunScrollView)findViewById(R.id.resultsScrollView);
 
-		int image_y = 0;
-		
-		int position = e621Search.offset;
-		
-		for (final E621Image img : e621Search.images)
+		new Thread(new Runnable()
 		{
-			LinearLayout resultWrapper = new LinearLayout(getApplicationContext());
-			ImageView imgView = new ImageView(getApplicationContext());
-			RelativeLayout rel = new RelativeLayout(getApplicationContext());
-			RelativeLayout imageWrapper = new RelativeLayout(getApplicationContext());
-			ProgressBar bar = new ProgressBar(getApplicationContext());
-			ImageButton download = new ImageButton(getApplicationContext());
-			RelativeLayout detailsWrapper = new RelativeLayout(getApplicationContext());
-			TextView details = new TextView(getApplicationContext());
-			
-			resultWrapper.setOrientation(LinearLayout.VERTICAL);
-			
-			ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
-					ViewGroup.LayoutParams.WRAP_CONTENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT));
-			resultWrapper.setLayoutParams(lp);
-			
-			lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
-					ViewGroup.LayoutParams.MATCH_PARENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT));
-			detailsWrapper.setLayoutParams(lp);
-			detailsWrapper.setBackgroundColor(getResources().getColor(R.color.detailsBackgroundColor));
-			
-			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				    RelativeLayout.LayoutParams.WRAP_CONTENT, 
-				    RelativeLayout.LayoutParams.WRAP_CONTENT);
-			params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-			details.setLayoutParams(params);
-			
-			String detailsText = "";
-			
-			if(img.score == 0)
+			public void run()
 			{
-				detailsText += "0";
-			}
-			else if(img.score > 0)
-			{
-				detailsText += "<font color=#00FF00>↑" + String.valueOf(img.score) + "</font>";
-			}
-			else if(img.score < 0)
-			{
-				detailsText += "<font color=#FF0000>↓" + String.valueOf(img.score) + "</font>";
-			}
-			
-			if(img.has_comments)
-			{
-				detailsText += " C";
-			}
-			
-			if(img.file_ext.equals("gif") || img.file_ext.equals("swf"))
-			{
-				detailsText += " A";
-			}
-			
-			if(img.rating.equals(E621Image.EXPLICIT))
-			{
-				detailsText += " <font color=#FF0000>E</font>";
-			}
-			else if(img.rating.equals(E621Image.QUESTIONABLE))
-			{
-				detailsText += " <font color=#FFFF00>Q</font>";
-			}
-			else if(img.rating.equals(E621Image.SAFE))
-			{
-				detailsText += " <font color=#00FF00>S</font>";
-			}
-			
-			details.setText(Html.fromHtml(detailsText));
-			
-			boolean has_border = false;
-			
-			if(img.has_children())
-			{
-				imageWrapper.setBackgroundResource(R.drawable.has_children_mark);
-				has_border = true;
-			}
-			else if(img.parent_id != null)
-			{
-				imageWrapper.setBackgroundResource(R.drawable.has_parent_mark);
-				has_border = true;
-			}
-			else if(img.status == E621Image.FLAGGED)
-			{
-				imageWrapper.setBackgroundResource(R.drawable.delete_mark);
-				has_border = true;
-			}
-			else if(img.status == E621Image.PENDING)
-			{
-				imageWrapper.setBackgroundResource(R.drawable.pending_mark);
-				has_border = true;
-			}
-
-			if(cur_min_id != null)
-			{
-				cur_min_id = String.valueOf(Math.min(Integer.parseInt(cur_min_id), Integer.parseInt(img.id)));
-			}
-			else
-			{
-				cur_min_id = img.id;
-			}
-
-			if(cur_max_id != null)
-			{
-				cur_max_id = String.valueOf(Math.max(Integer.parseInt(cur_max_id), Integer.parseInt(img.id)));
-			}
-			else
-			{
-				cur_max_id = img.id;
-			}
-			
-			int image_height = (int) (layout_width * (((double)img.preview_height) / img.preview_width));
-			
-			lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
-					layout_width,
-					image_height));
-			imgView.setLayoutParams(lp);
-			
-			params = new RelativeLayout.LayoutParams(
-				    RelativeLayout.LayoutParams.WRAP_CONTENT, 
-				    RelativeLayout.LayoutParams.WRAP_CONTENT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
-			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
-			download.setLayoutParams(params);
-			
-			if(e621.isSaved(img))
-			{
-				download.setImageResource(android.R.drawable.ic_menu_delete);
+				final ArrayList<View> views = new ArrayList<View>();
 				
-				download.setOnClickListener(new View.OnClickListener() {
-			        @Override
-			        public void onClick(View v) {
-			        	removeImage(img,(ImageButton)v);
-			        }
-			    });
-			}
-			else
-			{
-				download.setImageResource(android.R.drawable.ic_menu_save);
+				int position = e621Search.offset;
 				
-				download.setOnClickListener(new View.OnClickListener() {
-			        @Override
-			        public void onClick(View v) {
-			        	saveImage(img,(ImageButton)v);
-			        }
-			    });
-			}
-
-			resultWrapper.setPadding(0, dpToPx(10), 0, dpToPx(10));
-			if(has_border)
-			{
-				imageWrapper.setPadding(dpToPx(5),dpToPx(2),dpToPx(5),dpToPx(2));
-			}
-
-			imageViews.add(imgView);
-
-			imageWrapper.addView(bar);
-			imageWrapper.addView(imgView);
-			imageWrapper.addView(download);
-			rel.addView(imageWrapper);
-			resultWrapper.addView(rel);
-			
-			detailsWrapper.addView(details);
-			resultWrapper.addView(detailsWrapper);
-			
-			layout.addView(resultWrapper);
-
-			imgView.setTag(R.id.imagePosition, position);
-			imgView.setTag(R.id.imageObject, img);
-
-			imgView.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					imageClick(v);
+				for (final E621Image img : e621Search.images)
+				{
+					final LinearLayout resultWrapper = getResultWrapper(img,layout_width,position);
+					
+					position++;
+					
+					views.add(resultWrapper);
 				}
-			});
-
-			RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) bar
-					.getLayoutParams();
-			layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT,
-					RelativeLayout.TRUE);
-			bar.setLayoutParams(layoutParams);
-
-			ImageViewHandler handler = new ImageViewHandler(imgView, bar);
-			
-			scroll.addThread(new Thread(new ImageLoadRunnable(handler, img, e621,E621Image.PREVIEW)),image_y);
-			
-			image_y += image_height + 40;
-			
-			position++;
-		}
+				
+				runOnUiThread(new Runnable()
+				{
+					public void run()
+					{
+						int image_y = 0;
+						
+						for(int i=0; i<e621Search.images.size(); i++)
+						{
+							E621Image img = e621Search.images.get(i);
+							View resultWrapper = views.get(i);
+							
+							ImageView imgView = (ImageView) resultWrapper.findViewById(R.id.imageView);
+							ProgressBar progressBar = (ProgressBar) resultWrapper.findViewById(R.id.progressBar);
+							
+							layout.addView(resultWrapper);
+							ImageViewHandler handler = new ImageViewHandler(imgView, progressBar);
+							
+							scroll.addThread(new Thread(new ImageLoadRunnable(handler, img, e621,E621Image.PREVIEW)),image_y);
+							
+							imageViews.add(imgView);
+							
+							resultWrapper.measure(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+							image_y += resultWrapper.getMeasuredHeight() + dpToPx(10);
+						}
+					}
+				});
+			}
+		}).start();
 	}
 	
+	private LinearLayout getResultWrapper(E621Image img, int layout_width, int position)
+	{
+		LinearLayout resultWrapper = new LinearLayout(getApplicationContext());
+		RelativeLayout rel = new RelativeLayout(getApplicationContext());
+		RelativeLayout detailsWrapper = new RelativeLayout(getApplicationContext());
+		
+		resultWrapper.setOrientation(LinearLayout.VERTICAL);
+		
+		ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.WRAP_CONTENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT));
+		resultWrapper.setLayoutParams(lp);
+		
+		lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT));
+		detailsWrapper.setLayoutParams(lp);
+		detailsWrapper.setBackgroundColor(getResources().getColor(R.color.detailsBackgroundColor));
+		
+		TextView details = getImageFooter(img);
+		RelativeLayout imageWrapper = generateImageWrapper(img,layout_width,position);
+		updateCurMinMax(img);
+		
+		resultWrapper.setPadding(0, dpToPx(10), 0, dpToPx(10));
+		
+		rel.addView(imageWrapper);
+		resultWrapper.addView(rel);
+		
+		detailsWrapper.addView(details);
+		resultWrapper.addView(detailsWrapper);
+		
+		return resultWrapper;
+	}
+	
+	private ProgressBar generateProgressBar()
+	{
+		ProgressBar bar = new ProgressBar(getApplicationContext());
+
+		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT, 
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT,
+				RelativeLayout.TRUE);
+		bar.setLayoutParams(layoutParams);
+		
+		return bar;
+	}
+
+	private ImageView generateImageView(E621Image img, int layout_width, int position)
+	{
+		int image_height = (int) (layout_width * (((double)img.preview_height) / img.preview_width));
+		
+		ImageView imgView = new ImageView(getApplicationContext());
+		ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(new ViewGroup.LayoutParams(
+				layout_width,
+				image_height));
+		imgView.setLayoutParams(lp);
+		
+		imgView.setTag(R.id.imageObject, img);
+		imgView.setTag(R.id.imagePosition, position);
+
+		imgView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				imageClick(v);
+			}
+		});
+		
+		return imgView;
+	}
+
+	private RelativeLayout generateImageWrapper(E621Image img, int layout_width, int position)
+	{
+		RelativeLayout imageWrapper = new RelativeLayout(getApplicationContext());
+		
+		boolean has_border = false;
+		
+		if(img.has_children())
+		{
+			imageWrapper.setBackgroundResource(R.drawable.has_children_mark);
+			has_border = true;
+		}
+		else if(img.parent_id != null)
+		{
+			imageWrapper.setBackgroundResource(R.drawable.has_parent_mark);
+			has_border = true;
+		}
+		else if(img.status == E621Image.FLAGGED)
+		{
+			imageWrapper.setBackgroundResource(R.drawable.delete_mark);
+			has_border = true;
+		}
+		else if(img.status == E621Image.PENDING)
+		{
+			imageWrapper.setBackgroundResource(R.drawable.pending_mark);
+			has_border = true;
+		}
+		
+		if(has_border)
+		{
+			imageWrapper.setPadding(dpToPx(5),dpToPx(2),dpToPx(5),dpToPx(2));
+		}
+		
+		ProgressBar bar = generateProgressBar();
+		ImageView imgView = generateImageView(img,layout_width,position);
+		ImageButton download = generateDownloadButton(img);
+		
+		imgView.setId(R.id.imageView);
+		bar.setId(R.id.progressBar);
+		
+		imageWrapper.addView(bar);
+		imageWrapper.addView(imgView);
+		imageWrapper.addView(download);
+		
+		return imageWrapper;
+	}
+
+	private ImageButton generateDownloadButton(final E621Image img)
+	{
+		ImageButton download = new ImageButton(getApplicationContext());
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+			    RelativeLayout.LayoutParams.WRAP_CONTENT, 
+			    RelativeLayout.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+		download.setLayoutParams(params);
+		
+		if(e621.isSaved(img))
+		{
+			download.setImageResource(android.R.drawable.ic_menu_delete);
+			
+			download.setOnClickListener(new View.OnClickListener() {
+		        @Override
+		        public void onClick(View v) {
+		        	removeImage(img,(ImageButton)v);
+		        }
+		    });
+		}
+		else
+		{
+			download.setImageResource(android.R.drawable.ic_menu_save);
+			
+			download.setOnClickListener(new View.OnClickListener() {
+		        @Override
+		        public void onClick(View v) {
+		        	saveImage(img,(ImageButton)v);
+		        }
+		    });
+		}
+		
+		return download;
+	}
+
+	private void updateCurMinMax(E621Image img)
+	{
+		if(cur_min_id != null)
+		{
+			cur_min_id = String.valueOf(Math.min(Integer.parseInt(cur_min_id), Integer.parseInt(img.id)));
+		}
+		else
+		{
+			cur_min_id = img.id;
+		}
+
+		if(cur_max_id != null)
+		{
+			cur_max_id = String.valueOf(Math.max(Integer.parseInt(cur_max_id), Integer.parseInt(img.id)));
+		}
+		else
+		{
+			cur_max_id = img.id;
+		}
+	}
+
+	private TextView getImageFooter(E621Image img)
+	{
+		String detailsText = "";
+		
+		if(img.score == 0)
+		{
+			detailsText += "0";
+		}
+		else if(img.score > 0)
+		{
+			detailsText += "<font color=#00FF00>↑" + String.valueOf(img.score) + "</font>";
+		}
+		else if(img.score < 0)
+		{
+			detailsText += "<font color=#FF0000>↓" + String.valueOf(img.score) + "</font>";
+		}
+		
+		if(img.has_comments)
+		{
+			detailsText += " C";
+		}
+		
+		if(img.file_ext.equals("gif") || img.file_ext.equals("swf"))
+		{
+			detailsText += " A";
+		}
+		
+		if(img.rating.equals(E621Image.EXPLICIT))
+		{
+			detailsText += " <font color=#FF0000>E</font>";
+		}
+		else if(img.rating.equals(E621Image.QUESTIONABLE))
+		{
+			detailsText += " <font color=#FFFF00>Q</font>";
+		}
+		else if(img.rating.equals(E621Image.SAFE))
+		{
+			detailsText += " <font color=#00FF00>S</font>";
+		}
+		
+		TextView details = new TextView(getApplicationContext());
+		
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+			    RelativeLayout.LayoutParams.WRAP_CONTENT, 
+			    RelativeLayout.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+		details.setLayoutParams(params);
+		details.setText(Html.fromHtml(detailsText));
+		
+		return details;
+	}
+
 	public void saveImage(final E621Image img, final ImageButton v)
 	{
 		v.setImageResource(android.R.drawable.stat_sys_download);
