f117afde6481b8f3fc83b53b72fde282095a4446
Douglas Vanny Bento <douglas.vanny@gmail.com>
2015-05-01 15:39:11 -0300

    [#Minor Change - Debug Run Activity]
    Created Debug Run Activity for faster browsing while debuging

---
+++ app/src/main/java/info/beastarman/e621/frontend/RunDebugActivity.java
@@ -0,0 +1,29 @@
+package info.beastarman.e621.frontend;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+
+public class RunDebugActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        boolean isDebuggable =  ( 0 != ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE ) );
+
+        Intent intent = new Intent(this,MainActivity.class);
+
+        if(isDebuggable)
+        {
+            intent = new Intent(this,SearchActivity.class);
+            intent.putExtra(SearchActivity.SEARCH,"type:webm");
+        }
+
+        startActivity(intent);
+
+        finish();
+    }
+}
